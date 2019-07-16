91260a9acebe56bad0556361c735dfa8574fd407
brzeroquarenta <grito@Gritos-iMac-2.local>
2019-07-16 17:07:12 -0300

    Criado base do projeto em react

--- index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use('/', express.static('htdocs'));
-
-app.listen(3000, function() {
-	console.log('listen on http://localhost:3000');
-});

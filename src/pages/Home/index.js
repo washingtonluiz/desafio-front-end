c3743acc19a7f56bc0cc733ab0d3893d218f6adb
brzeroquarenta <grito@Gritos-iMac-2.local>
2019-07-16 18:40:48 -0300

    Criado base da Api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Api from '../../services/Api';
 import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Home</h1>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      main: {},
+      brasil: {},
+      mundo: {},
+    }
+  }
+  getData = async () => {
+    this.setState({main: Api.section[0], brasil: Api.section[1], mundo: Api.section[2]})
+  }
+
+  componentDidMount(){
+    this.getData();
+  }
+  render() {
+    console.log(this.state.main)
+    console.log(this.state.brasil)
+    console.log(this.state.mundo)
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
